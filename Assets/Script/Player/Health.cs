5e08b28cda56b0c9f1a6f5721c20b03646a105da
erwush <fachry0107.mc@gmail.com>
2026-01-28 14:47:50 +0700

    0.0.18.7-alpha
    - Player now has iFrame (invincibility Frame)
    - Player now has Death Animation (or effect)

--- Assets/Script/Player/Health.cs
+++ Assets/Script/Player/Health.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class Health : MonoBehaviour
 {
@@ -7,12 +8,17 @@ public class Health : MonoBehaviour
     public float maxHp;
     private Checkpoint checkpoint;
     private Combat combat;
+    private Movement movement;
+    private bool iFrame;
+    private Collider2D col;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         checkpoint = GetComponent<Checkpoint>();
         combat = GetComponent<Combat>();
+        movement = GetComponent<Movement>();
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -23,25 +29,50 @@ public class Health : MonoBehaviour
 
     public void HealthChange(float amount)
     {
-        if(amount < 0 && !combat.isParry)
+        if (!iFrame)
         {
-            hp += amount;   
-        } else if (combat.isParry)
-        {
-            combat.parryTimer = 0f;
+            if (amount < 0 && !combat.isParry)
+            {
+                hp += amount;
+                StartCoroutine(InvFrame());
+            }
+            else if (combat.isParry)
+            {
+                combat.parryTimer = 0f;
+            }
         }
-        if(amount> 0)
+        if (amount > 0)
         {
             hp += amount;
         }
         if (hp <= 0)
         {
-            hp = maxHp;
-            checkpoint.Respawn();
+            StartCoroutine(Death());
         }
-        if(hp > maxHp)
+        if (hp > maxHp)
         {
             hp = maxHp;
         }
     }
+
+    IEnumerator InvFrame()
+    {
+        iFrame = true;
+        yield return new WaitForSeconds(0.01f);
+        iFrame = false;
+    }
+
+    public IEnumerator Death()
+    {
+        iFrame = true;
+        movement.Jump(25f, false);
+        col.isTrigger = true;
+        yield return new WaitForSeconds(1.15f);
+        hp = maxHp;
+        col.isTrigger = false;
+        checkpoint.Respawn();
+        iFrame = false;
+
+    }
+
 }
