79bc2ad3f055095171c2bf5579ee462cdf67c7e9
erwush <fachry0107.mc@gmail.com>
2026-03-01 21:07:38 +0700

    nganu

--- Assets/Script/Enemy/KrocoCombat.cs
+++ Assets/Script/Enemy/KrocoCombat.cs
@@ -1,6 +1,101 @@
+using System.Collections;
 using UnityEngine;
 
-public class KrocoCombat : EnemyCombat
+public  class KrocoCombat : EnemyCombat
 {
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        movement = GetComponent<EnemyMovement>();
+        selfSprite = GetComponent<SpriteRenderer>();
+        defaultMaterial = selfSprite.material;
+        canAttack = true;
+        rb =  GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.UpdateBar(hp, maxHp);
+        if (atkTimer > 0)
+        {
+            atkTimer -= Time.deltaTime;
+        }
+        // if (isHit)
+        // {
+
+        // }
+        // else if (!isHit)
+        // {
+
+        // }
+    }
+
+
+
+
+
+
+
+    public  override void FinishAttacking()
+    {
+        if (anim.GetBool("isAttacking1") == true)
+        {
+            anim.SetBool("isAttacking1", false);
+
+
+        }
+    }
+
+    public override void Attack()
+    {
+
+        Collider2D[] hits = Physics2D.OverlapCircleAll(atkPoint.position, atkRange, pLayer);
+        atkTimer = atkCd;
+        if (PlayerCombat == null)
+        {
+            PlayerCombat = hits[0].gameObject.GetComponent<Combat>();
+
+        }
+        if (hits.Length > 0)
+        {
+            float demeg;
+            demeg = atk;
+            hits[0].GetComponent<Health>().HealthChange(-demeg);
+            // Debug.Log("demeg musuh:" + demeg);
+
+        }
+    }
+
+    public override void HealthChange(float hpAmount)
+    {
+        if (hpAmount < 0)
+        {
+            StartCoroutine(HurtAnim());
+        }
+
+
+        hp += hpAmount;
+
+
+        healthBar.UpdateBar(hp, maxHp);
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(atkPoint.position, atkRange);
+    }
+
 
 }
--- Assets/Script/Enemy/KrocoMovement.cs
+++ Assets/Script/Enemy/KrocoMovement.cs
@@ -2,5 +2,59 @@ using UnityEngine;
 
 public class KrocoMovement : EnemyMovement
 {
-    
+  
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        stat = GetComponent<EnemyCombat>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForPlayer();
+        if (enemyState == EnemyState.Chasing)
+        {
+            if (canMove)
+            {
+                Chase();
+            }
+        }
+        else if (enemyState == EnemyState.Attacking && !unstop)
+        {
+            rb.linearVelocity = Vector2.zero;
+
+        }
+        if (!canMove && !unstop)
+        {
+            rb.linearVelocity = Vector2.zero;
+            ChangeState(EnemyState.Idle);
+            //chagne color to red
+        }
+
+    }
+
+    void Chase()
+    {
+        // Flip arah hadap
+        if ((player.position.x > transform.position.x && facingDirection == -1) ||
+            (player.position.x < transform.position.x && facingDirection == 1))
+        {
+            Flip();
+        }
+
+        // Gerak hanya di X
+        float dirX = Mathf.Sign(player.position.x - transform.position.x);
+        rb.linearVelocity = new Vector2(dirX * spd, rb.linearVelocity.y);
+
+    }
+
+  
 }
+
+
+
+
+
